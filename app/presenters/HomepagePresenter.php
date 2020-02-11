f4dcb6e4e1b0fc7e26339a2c66af14fa97aafee7
Roman Janko <admin@rjwebdesign.cz>
2020-02-11 15:12:12 +0100

    Ukazka formulare, presmerovani + FM

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,8 +5,24 @@ namespace App\Presenters;
 
 final class HomepagePresenter extends BasePresenter
 {
+
+
+	public function actionDefault()
+	{
+		$this['testForm']['form']->onSuccess[] = function ($form, $values) {
+
+			$this->flashMessage('Odeslano');
+			$this->redirect('this');
+		};
+	}
+
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
 	}
+
+
+
+
+	
+
 }
